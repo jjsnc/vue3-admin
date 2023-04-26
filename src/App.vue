0d1c15fa987aa6e12d17cf19fff255e948673973
早起喝燕麦 <zhuowangyi@163.com>
2023-04-26 15:07:09 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <HelloWorld msg="Vite + Vue" />
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
 </template>
 
 <style scoped>
